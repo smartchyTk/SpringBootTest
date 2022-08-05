f8766d386617d77a168ac6eb67216abcac6e8e25
smartchyTk <110071349+smartchyTk@users.noreply.github.com>
2022-08-05 17:56:02 +0900

    빌드오류 수정
    빌드오류 수정

--- src/test/java/com/Spring/springBootTest/SpringBootTestApplicationTests.java
+++ src/test/java/com/Spring/springBootTest/SpringBootTestApplicationTests.java
@@ -1,9 +1,9 @@
 package com.Spring.springBootTest;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringBootTestApplicationTests {
 
 	@Test
